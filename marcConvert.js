3b4efdd76c2d4eb75de4484c93561ed9ac381a01
nankj <35960024+nankj@users.noreply.github.com>
2022-01-04 10:19:51 +0000

    Add support for Chrome and FF
    This commit also normalizes combining diacritics

--- marcConvert.js
+++ marcConvert.js
@@ -4,6 +4,7 @@ function convertAndCopy() {
 	var outputTextarea = document.getElementById('output');
 	
 	var button = document.getElementById('btn');
+	var btnClicked = document.getElementsByClassName('btn_clicked');	
 
 // return focus to input if nothing there to work on
 	if (!inputText) {
@@ -11,17 +12,26 @@ function convertAndCopy() {
 		return;
 	}
 // transform inputText
-	var outputText =   inputText.replace(/[^\n][ \t]+(\d{3}) \t +\t(..)[ \t]+(.*)/g,'$1$2$3')
-								.replace(/ ‡(.) /g,'|$1')
-								.replace(/_‡(.) /g,'_|$1')
-								.replace(/(\n\d{3,4})_/g,'$1 ');
+	var outputText = inputText.replace(/ +\t(\d{3})\t? \t(?:  \t)?([\d\_]{2}|  )[ \t]+(.*)/gm,'$1$2$3')
+				  			  .replace(/ ‡(.) /g,'|$1')
+				  			  .replace(/_‡(.) /g,'_|$1')
+				  			  .replace(/(\d{3,4})_/g,'$1 ');
 	
+// try and decompose combining diacritics into single characters
+	outputText = outputText.normalize("NFC");
+
 	outputTextarea.textContent = outputText;
 	outputTextarea.select();
 	button.innerText = 'Copied! Click to reset';
+
+// only copy contents if this is first time clicking
+	if (btnClicked.length == 0) {
+		document.execCommand('copy');
+		button.classList.add('btn_clicked');
+	}	
+
 // hard reload of page to get around bug where user interactions with output textarea break things
 	button.addEventListener('click', (function(){window.location.reload(true)}));
-	document.execCommand('copy');
 	
 	return;
 }
